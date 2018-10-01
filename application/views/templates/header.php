512f707ac66a123bab2a0760376f6149d63791ad
AKokoshyan <akokoshqn@gmail.com>
2018-10-01 08:19:33 +0300

    Issue fixed: new bootstrap template matching bootstrap 4.0 was added

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
     <head>
-        <title>Codeigniter</title>
-        <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+    <title>Codeigniter</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
 
     <body>
 
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
+
+<nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="<?php echo base_url(); ?>">Codeigniter</a>
+      <a class="navbar-brand" href="<?php base_url(); ?>">Codeigniter</a>
     </div>
-    <div id = "navbar">
     <ul class="nav navbar-nav">
-      <li><a href="<?php echo base_url(); ?>">Home</a></li>
-      <li><a href="<?php echo base_url(); ?>about">About</a></li>
+      <li><a href="<?php base_url(); ?>">Home</a></li>
+      <li><a href="<?php base_url(); ?>about">About</a></li>
     </ul>
-    </div>
   </div>
 </nav>
